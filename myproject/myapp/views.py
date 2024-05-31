f52e4d49563f3fe79f89ac72cc243a60c5882ee3
rio0232 <rio.saito@goodsun-hd.com>
2024-05-31 17:09:34 +0900

    viewの編集

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from myapp.models import Movie, Director, Log
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'myapp/index.html'
+    context_object_name = 'movie_list'
+    queryset = Movie.object.all()
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    template_name = 'myapp/detail.html'
+
+
