bef36f6e37d4e8e39f4c26d0dad3d85295b1cec8
rio0232 <rio.saito@goodsun-hd.com>
2024-06-03 16:35:15 +0900

    viewの編集

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,21 +3,21 @@ from django.utils import timezone
 
 class Director(models.Model):
     name = models.CharField(max_length=100, verbose_name="監督")
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 class Movie(models.Model):
     title = models.CharField(max_length=100, verbose_name="タイトル")
-    watch_data = models.DateField()
+    watch_date = models.DateField()
     director = models.ForeignKey(Director, on_delete=models.CASCADE, verbose_name="監督", related_name='movie')
-    def _str_(self):
-        return self.name
+    def __str__(self):
+        return self.title
 
 class Log(models.Model):
     text = models.TextField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name="タイトル", related_name='log')
 
-    def _str_(self):
-        return self.name
+    def __str__(self):
+        return self.text
 
 # Create your models here.
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from myapp.models import Movie, Director, Log
 from django.views import generic
+from myapp.models import Movie, Director, Log 
+from django.shortcuts import render
+from django.urls import reverse
+
+
 
 class IndexView(generic.ListView):
     template_name = 'myapp/index.html'
     context_object_name = 'movie_list'
-    queryset = Movie.object.all()
+    queryset = Movie.objects.all()
 
 class MovieDetailView(generic.DetailView):
     model = Movie
     template_name = 'myapp/detail.html'
+    context_object_name = 'movie'
 
 
+class RegisterDirectorView(generic.CreateView):
+    model = Director
+    form_class = DirectorForm
+    template_name = 'myapp/register.html'
+    def get_success_url(self):
+        return reverse('myapp:registermovie') 
+
+
+class RegisterMovieView(generic.CreateView):
+    model = Movie
+    form_class = MovieForm
+    template_name = 'myapp/register.html'
+    def get_success_url(self):
+        return reverse('myapp:movie_detail', kwargs={'pk': self.object.pk }) 
+
+class WritingLogView(generic.CreateView):
+    model = Log
+    form_class = LogForm
+    template_name = 'myapp/register.html'
+    def get_success_url(self):
+        return reverse('myapp:movie_detail', kwargs={'pk': self.object.movie.pk }) 
