322c51778f8654dd0f2e0f74aae58bf067fcd051
Hironori43 <hironori.ito@goodsun-hd.com>
2024-06-06 16:13:35 +0900

    new login signup

--- djangotodo/config/urls.py
+++ djangotodo/config/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 # Todoアプリ
 from django.contrib import admin
 from django.urls import path, include
-
-from django.contrib import admin
-from django.urls import path, include
  
 
 urlpatterns = [
--- djangotodo/login_app/views.py
+++ djangotodo/login_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 # Create your views here.
 def signup_view(request):
-    pass
+    if request.method == 'POST':
+        
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    else:
+        form = SignupForm()
+        
+    param = {
+        'form':form
+    }
+    return render(request, 'login_app/signup.html', param)
 
 def login_view(request):
     pass
