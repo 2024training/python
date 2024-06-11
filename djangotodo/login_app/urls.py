b2127d7b55b51cdc7ebae2351ace885326d704d1
Hironori43 <hironori.ito@goodsun-hd.com>
2024-06-11 17:29:31 +0900

    bug fix

--- djangotodo/login_app/urls.py
+++ djangotodo/login_app/urls.py
@@ -5,9 +5,6 @@ from todo.views import TodoCreate
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
     path('logout/', views.logout_confirm, name='logout_confirm'),
-    path('user/', views.user_view, name='user'),
-    path('other/', views.other_view, name='other'),
     path('create/', TodoCreate.as_view(), name='todo_create'),
 ]
