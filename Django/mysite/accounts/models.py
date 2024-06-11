b2a6ee89f3ab01ac5d40eecba80998e33a323f0c
KazumaMorozumi <kazuma.morozumi@goodsun-hd.com>
2024-06-11 09:27:02 +0900

    checkout

---
+++ Django/mysite/accounts/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+from django.contrib.auth.models import UserManager
+
+class CustomUserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, password, **extra_fields):
+        # create_user と create_superuser の共通処理
+        if not email:
+            raise ValueError('email must be set')
+        if not username:
+            raise ValueError('username must be set')
+
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, username, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+
+    # 関連名を変更する
+    groups = models.ManyToManyField(Group, related_name='custom_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_users')
+
+    def __str__(self):
+        return self.email
+
+# 関連名を変更するためにデフォルトのユーザーモデルを上書きする
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# この1文を消すことが果たしてできるのかね？
+# なんかそもそもこの更新が反映されないんですけど。
