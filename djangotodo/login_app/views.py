7dd74a695d3dc1d554b1a3015297057bf8820108
Hironori43 <hironori.ito@goodsun-hd.com>
2024-06-11 17:17:31 +0900

    編集アップデート、コメント追加

--- djangotodo/login_app/views.py
+++ djangotodo/login_app/views.py
@@ -6,15 +6,22 @@ from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
+    # POSTかチェック
     if request.method == 'POST':
+        # フォームオブジェクトを作成
         form = SignupForm(request.POST)
+        # 入力した要件を満たしているかチェック
         if form.is_valid():
             user = form.save()
-            login(request, user)  # アカウント登録後に自動的にログインする
-            return redirect(reverse_lazy('login'))  # ログインページにリダイレクトする
+            # アカウント登録後に自動的にログイン
+            login(request, user) 
+            # ログインページにリダイレクト
+            return redirect(reverse_lazy('login'))
     else:
+        # 空のフォームを作成
         form = SignupForm()
-        
+    
+    # フォームを送信していないとき新規登録ページを表示    
     param = {
         'form': form
     }
@@ -22,15 +29,18 @@ def signup_view(request):
 
 def login_view(request):
     if request.method == 'POST':
+        # ユーザーの入力したデータをフォームに結びつける(バインド)
         form = LoginForm(request, data=request.POST)
-        
+        # フォームが有効かチェック
         if form.is_valid():
             user = form.get_user()
             
             if user:
                 login(request, user)
-                next_page = request.GET.get('next', reverse_lazy('list'))  # 'next' パラメータからリダイレクト先を取得
-                return redirect(next_page)  # リダイレクト先を指定してリダイレクト
+                # 'next' パラメータからリダイレクト先を取得
+                next_page = request.GET.get('next', reverse_lazy('list'))
+                # リダイレクト先を指定してリダイレクト
+                return redirect(next_page)
                 
     else:
         form = LoginForm()
@@ -41,24 +51,14 @@ def login_view(request):
 
     return render(request, 'login_app/login.html', param)
 
-# def logout_view(request):
-#     logout(request)
-    
-#     return render(request, 'login_app/logout.html')
-
 def logout_confirm(request):
     if request.method == 'POST':
         logout(request)
-        return redirect('login')  # ログインページにリダイレクト
+        # ログインページにリダイレクト
+        return redirect('login')
     return render(request, 'login_app/logout_confirm.html')
 
 def logout_view(request):
     logout(request)
     
     return render(request, 'login_app/logout.html')
-
-def user_view(request):
-    pass
-
-def other_view(request):
-    pass
--- djangotodo/todo/views.py
+++ djangotodo/todo/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# ユーザーがログインしていることを確認。ログインしていないユーザーがToDoリストにアクセスしようとすると、ログインページにリダイレクトされる。
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-
 from .models import Todo
-from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
 
 class TodoList(LoginRequiredMixin, ListView):
+    # このビューで作成するモデルの指定
     model = Todo
+    # テンプレート内では、ToDoリストがtasksという名前で利用可能
     context_object_name = "tasks"
     
     def get_queryset(self):
@@ -17,20 +17,27 @@ class TodoList(LoginRequiredMixin, ListView):
     
 class TodoDetail(DetailView):
         model = Todo
+        # テンプレート内では、ToDoリストがtaskという名前で利用可能
         context_object_name = "task"
         
 class TodoCreate(LoginRequiredMixin, CreateView):
     model = Todo
+    # ユーザーに入力させるフィールド指定
     fields = ['title', 'description', 'deadline']
+    # Todoが正常に作成された後Listにリダイレクト
     success_url = reverse_lazy('list')
+    # ビューで使うテンプレートの名前指定
     template_name = 'todo/todo_form.html'
 
     def form_valid(self, form):
+        # 現在ログインしているユーザーの情報設定
         form.instance.user = self.request.user
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
+        # context_dataメソッドの結果をcontextに格納
         context = super().get_context_data(**kwargs)
+        # actionキーにcreateを割り当て
         context['action'] = 'create'
         return context
     
@@ -42,7 +49,10 @@ class TodoUpdate(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # actionキーにupdateを割り当て
         context['action'] = 'update'
+        # ビューで更新されるTodoインスタンスの取得、ここで現在のTodoを渡す
+        context['task'] = self.get_object() 
         return context
     
 class TodoDelete(DeleteView):
