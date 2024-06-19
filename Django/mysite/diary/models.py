3eb4993ea2abbc45389d33da5dd72fa2b21efd4d
KazumaMorozumi <kazuma.morozumi@goodsun-hd.com>
2024-06-19 14:01:27 +0900

    テストコードを記述

--- Django/mysite/diary/models.py
+++ Django/mysite/diary/models.py
@@ -13,6 +13,9 @@ class Diary(models.Model):
     updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
     # 外部キー　ユーザーの消去に伴って日記データも消すという優れもの
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # 文字列表現をタイトルに指定(管理画面で見やすく)(表示やデバッグのための機能なので、データベースクエリの検索条件にはなりえない)
+    def __str__(self):
+        return f"{self.text}({self.created_at.year}年{self.created_at.month}月{self.created_at.day}日)"
 
 
 
--- Django/mysite/diary/tests.py
+++ Django/mysite/diary/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+# import datetime
+# from django.test import TestCase
+# from .models import Diary
+# from django.contrib.auth.models import User
 
-# Create your tests here.
+# class DiaryModelTests(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username='testuser', password='password')
+
+#     def test_diary_creation(self):
+#         '''ユーザーが設定されたか、タイトル、本文が正しいか'''
+#         Diary.objects.create(user=self.user, title='A', text='B')
+#         diary = Diary.objects.get(title='A')
+
+#         self.assertEqual(diary.title, 'A')
+#         self.assertEqual(diary.text, 'B')
+#         self.assertEqual(diary.user, self.user)
+
+#     def test_diary_has_date(self):
+#         '''本文が正しいか、日付が設定されているか'''
+#         Diary.objects.create(user=self.user, title='C', text='D')
+#         diary = Diary.objects.get(title='C')
+#         self.assertEqual(diary.text, 'D')
+#         self.assertIsInstance(diary.date, datetime.date)
+
+#     def test_save_and_retrieve(self):
+#         '''タイトルが正しいか、本文が正しいか'''
+#         Diary.objects.create(user=self.user, title='E', text='F')
+#         diary = Diary.objects.get(title='E')
+#         self.assertEqual(diary.title, 'E')
+#         self.assertNotEqual(diary.title, 'zzz')
+#         self.assertEqual(diary.text, 'F')
+
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from .models import Diary
+
+class DiaryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # テストに必要な初期データのセットアップ
+        user = get_user_model().objects.create_user(username='testuser', password='12345')
+        Diary.objects.create(title='Test Diary', text='This is a test diary entry', user=user)
+
+    def test_diary_content(self):
+        '''タイトルの確認'''
+        diary = Diary.objects.get(title='Test Diary')
+        expected_object_name = f'{diary.title}'
+        self.assertEqual(expected_object_name, 'Test Diary')
+
+    def test_diary_date_default(self):
+        '''日付の確認'''
+        diary = Diary.objects.get(title='Test Diary')
+        expected_date = timezone.now().date()
+        self.assertEqual(diary.date, expected_date)
+
+    def test_diary_updated_at_blank_null(self):
+        '''更新日時の確認'''
+        diary = Diary.objects.get(title='Test Diary')
+        self.assertIsNone(diary.updated_at)
+
+    def test_diary_user_relationship(self):
+        '''usernameの確認'''
+        diary = Diary.objects.get(title='Test Diary')
+        self.assertEqual(diary.user.username, 'testuser')
+
+    def test_diary_str_method(self):
+        '''文字列表現の確認'''
+        diary = Diary.objects.get(title='Test Diary')
+        self.assertEqual(str(diary), diary.title)
